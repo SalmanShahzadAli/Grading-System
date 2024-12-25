e5d2100258bb0b46357f4a5b3e5ed1aed89e06bf
Salman <salmanshahzad91@gmail.com>
2024-12-25 22:30:33 +0500

    f

--- InputModule2.py
+++ InputModule2.py
@@ -6,8 +6,8 @@
 # define the grade thresholds (e.g., >=90% = A).
 
 def Grade_Threshold_Absolute():
-    print("Please Define The Thresholds For Grades"),
-    print("We Have The Following Grades [A,B,C,D,F]"),
+    print("Please Define The Thresholds For Grades")
+    print("We Have The Following Grades [A,B,C,D,F]")
     
     # Define thresholds for absolute grading
     A = int(input("Define Threshold For (e.g., >= 90%) A: "))
@@ -23,8 +23,18 @@ def Grade_Threshold_Absolute():
         'C': C,
         'D': D,
         'F': F,
-    },
+    }
     
-    print("Grade thresholds defined as follows:"),
+    print("Grade thresholds defined as follows:")
     for grade, threshold in thresholds.items():
         print(f"{grade}: >= {threshold}%")
+    print("For Example IF A Student Scores More Than 90% Than He Would For Sure Get An A Grade")
+    print("For Example IF A Student Scores Less Than 60% Then He Would For Sure Get An F Grade")    
+
+def main():
+    Grade_Threshold_Absolute()
+    
+
+# Run the main function
+if __name__ == "__main__":
+    main()    
