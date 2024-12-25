2f416766f6dde48a0aca68f130df58d8c7810a1f
Salman <salmanshahzad91@gmail.com>
2024-12-25 22:05:36 +0500

    fourth

--- InputModule1.py
+++ InputModule1.py
@@ -1,1 +1,10 @@
-# in this module
+# in this module we are giving the user an option to 
+# select between relative and absolute grading
+
+def select_grading_policy():
+    print("Please Select Between Relative And Absolute Grading")
+    print()
+    grading_policy = input("Please Enter The Name OF The Grading Policy")
+
+def main():
+    select_grading_policy()    
